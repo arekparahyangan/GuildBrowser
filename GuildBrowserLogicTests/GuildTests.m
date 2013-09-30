efe60c41fe298983bba3a2d9afd337b3d3bf0216
arekparahyangan <sofyan@Sofyans-MacBook-Air.local>
2013-09-30 17:25:37 +0700

    Added the skeleton of a new test class GuildTests and the supporting TestSemaphor class.

---
+++ GuildBrowserLogicTests/GuildTests.m
@@ -0,0 +1,39 @@
+//
+//  GuildTests.m
+//  GuildBrowser
+//
+//  Created by Sofyan Nugroho on 9/30/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "WoWApiClient.h"
+#import <OCMock/OCMock.h> 
+#import "Guild.h"
+#import "TestSemaphor.h" 
+#import "Character.h"
+
+@interface GuildTests : XCTestCase
+
+@end
+
+@implementation GuildTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
